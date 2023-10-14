8e445c5957e44889c2ea516c831637d23adf9b26
Rednelg <glenderyanez@gmail.com>
2023-10-14 11:49:25 +0200

    netlify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import serverless from "serverless-http";
 import {router as productRouter} from "./routes/product.routes.js"
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use('/api',productRouter)
+
+export const handler = serverless(app);
 
 // eslint-disable-next-line no-undef
 app.listen(process.env.PORT || app.get('PORT'),()=>{
